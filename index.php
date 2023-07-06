717b0bc5088a6a7d5a527f65ba7721f56338ab19
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-07-07 01:19:34 +0200

    created articol with v-for of discs array

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Our first API</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <div id="app">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <article class="disc" v-for="(disc, index) disc in discs" :key="index">
+
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="./script/main.js"></script>
+    </body>
+</html>
